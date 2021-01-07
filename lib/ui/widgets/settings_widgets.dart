c4cf65c001455cc270ad6679bfbbdb6a5d2faa17
brandonmkunkel <brandonmkunkel@gmail.com>
2021-01-07 09:43:33 -0500

    Missing settings_widgets file from previous commits.

---
+++ lib/ui/widgets/settings_widgets.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'package:auto_call/services/settings_manager.dart';
+
+/// Boolean Settings Widget
+class BoolSettingsWidget extends StatefulWidget {
+  final String settingKey;
+  final Setting setting;
+
+  BoolSettingsWidget({Key key, @required this.settingKey, @required this.setting});
+
+  @override
+  BoolSettingsState createState() => new BoolSettingsState();
+}
+
+class BoolSettingsState extends State<BoolSettingsWidget> {
+  Setting setting;
+
+  @override
+  void initState() {
+    setting = widget.setting;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.setting.text),
+      trailing: Switch(
+        value: widget.setting.value,
+        onChanged: (bool value) {
+          setState(() {
+            globalSettingManager.setSetting(widget.settingKey, value);
+          });
+        },
+      ),
+    );
+  }
+}
