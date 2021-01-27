8d383cd5309b35987659a8a1c1af31c5962e98e2
Brandon Kunkel <38381956+brandonmkunkel@users.noreply.github.com>
2021-01-26 22:42:03 -0500

    iOS version updated to work correctly with Theming and Firebase

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -24,8 +24,8 @@ class ThemeProvider with ChangeNotifier {
 // This file will hold information describing the Theme for the App
 final ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    backgroundColor: Colors.grey[300],
-    primarySwatch: defaultTargetPlatform == TargetPlatform.iOS ? Colors.white : Colors.green,
+    backgroundColor: Colors.grey[00],
+    primarySwatch: defaultTargetPlatform == TargetPlatform.iOS ? Colors.grey : Colors.green,
     disabledColor: Colors.grey[400],
     fontFamily: "Raleway"
 );
@@ -34,7 +34,7 @@ final ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     backgroundColor: Colors.grey[900],
     scaffoldBackgroundColor: Colors.grey[900],
-    primarySwatch: defaultTargetPlatform == TargetPlatform.iOS ? Colors.grey[700] : Colors.green,
+    primarySwatch: defaultTargetPlatform == TargetPlatform.iOS ? Colors.grey : Colors.green,
     accentColor: Colors.green[600],
     disabledColor: Colors.grey[700],
     fontFamily: "Raleway"
